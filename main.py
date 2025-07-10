ac61d6f12e61a5e3dd135af21fe6f8cb2809b019
DmitriiMurovtsev <mdv12672@gmail.com>
2025-07-10 12:25:37 +0300

    Снизил пороговое значение score_threshold в функции поиска с 0.40 до 0.30 для улучшения фильтрации результатов

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ async def search(request: Request) -> List[Dict[str, str]]:
             collection_name=COLLECTION_NAME,
             query_vector=embedding,
             limit=top,
-            score_threshold=0.40,
+            score_threshold=0.30,
         )
         logger.info(f'Вопрос: {query}\nРезультат поиска: {results}')
         return [r.payload for r in results]
